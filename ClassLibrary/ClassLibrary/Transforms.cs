5dfa415e255a20cb1b3b72f7cbef2d93fa6b71be
Nikita <nike04@mail.ru>
2020-11-05 03:59:41 +0500

    first commit

---
+++ ClassLibrary/ClassLibrary/Transforms.cs
@@ -0,0 +1,37 @@
+﻿using PluginInterface;
+using System.Drawing;
+
+
+namespace ClassLibrary
+{
+    class Transforms
+    {
+        public string Name
+        {
+            get
+            {
+                return "Переворот изображения";
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return "Me";
+            }
+        }
+
+        public void Transform(Bitmap bitmap)
+        {
+            for (int i = 0; i < bitmap.Width; ++i)
+                for (int j = 0; j < bitmap.Height / 2; ++j)
+                {
+                    Color color = bitmap.GetPixel(i, j);
+                    bitmap.SetPixel(i, j, bitmap.GetPixel(i, bitmap.Height - j - 1));
+                    bitmap.SetPixel(i, bitmap.Height - j - 1, color);
+                }
+        }
+
+    }
+}
