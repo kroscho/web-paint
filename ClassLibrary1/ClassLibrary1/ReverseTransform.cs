a0857a2c40d0063e0f31ae67bca68f3c2de31ba0
Nikita <nike04@mail.ru>
2020-11-11 00:41:29 +0500

    yes

---
+++ ClassLibrary1/ClassLibrary1/ReverseTransform.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using PluginInterface;
+using System.Drawing;
+
+
+namespace ClassLibrary1
+{
+    [Version(1, 0)]
+    public class ReverseTransform : IPlugin
+    {
+        public string Name
+        {
+            get
+            {
+                return "Переворот изображения";
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return "Kros";
+            }
+        }
+
+        public void Transform(IMainApp app)
+        {
+            var bitmap = app.Image;
+            for (int i = 0; i < bitmap.Width; ++i)
+            {
+                for (int j = 0; j < bitmap.Height / 2; ++j)
+                {
+                    Color color = bitmap.GetPixel(i, j);
+                    bitmap.SetPixel(i, j, bitmap.GetPixel(i, bitmap.Height - j - 1));
+                    bitmap.SetPixel(i, bitmap.Height - j - 1, color);
+                }
+            }
+            app.Image = bitmap;
+        }
+    }
+
+}
+
